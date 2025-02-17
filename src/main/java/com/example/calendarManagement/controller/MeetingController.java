ba79811931d52fff6333890f4521a95212826fd1
ajay2728-dev <ajay.singh@capillarytech.com>
2025-02-17 11:58:59 +0530

    thrift server call is made

--- src/main/java/com/example/calendarManagement/controller/MeetingController.java
+++ src/main/java/com/example/calendarManagement/controller/MeetingController.java
@@ -1,10 +1,36 @@
 package com.example.calendarManagement.controller;
 
+import com.example.calendarManagement.dto.ResponseDTO;
+import com.example.calendarManagement.service.MeetingService;
+import com.example.thriftMeeting.IMeetingServiceDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.thrift.TException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
 @RestController
 @RequestMapping("/api/meeting")
 public class MeetingController {
 
+    @Autowired
+    MeetingService meetingService;
+
+    @PostMapping("/schedule")
+    public ResponseEntity<ResponseDTO> canScheduleMeeting(@RequestBody IMeetingServiceDTO meetingServiceDTO) throws TException {
+        log.info("can schedule meeting");
+        Object body = meetingService.canScheduleMeeting(meetingServiceDTO);
+
+        Map<String, Object> data = new HashMap<>();
+        data.put("body",body);
+        ResponseDTO responseBody = new ResponseDTO("Meeting can be scheduled",200,data,null);
+        return ResponseEntity.ok(responseBody);
+    }
 }
--- src/main/java/com/example/calendarManagement/service/MeetingService.java
+++ src/main/java/com/example/calendarManagement/service/MeetingService.java
@@ -1,4 +1,44 @@
 package com.example.calendarManagement.service;
 
+import com.example.thriftMeeting.IMeetingService;
+import com.example.thriftMeeting.IMeetingServiceDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.thrift.TException;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.transport.TSocket;
+import org.apache.thrift.transport.TTransport;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@Service
 public class MeetingService {
+    private final IMeetingService.Client client;
+
+    public MeetingService(){
+        try {
+            log.info("meeting thrift ...");
+            TTransport transport = new TSocket("localhost", 9090);
+            transport.open();
+            TBinaryProtocol protocol = new TBinaryProtocol(transport);
+            client = new IMeetingService.Client(protocol);
+        } catch (Exception e) {
+            throw new RuntimeException("Error initializing Thrift client", e);
+        }
+    }
+
+    public Object canScheduleMeeting(IMeetingServiceDTO meetingServiceDTO) throws TException {
+        try {
+            boolean response = client.canScheduleMeeting(meetingServiceDTO);
+            Map<String, Object> data = new HashMap<>();
+            data.put("canSchedule",response);
+            return data;
+        } catch ( RuntimeException | TException ex){
+            throw new RuntimeException(ex);
+        }
+
+    }
+
 }
